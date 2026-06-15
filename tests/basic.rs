1004489be1454fa973c4140f531f8cc993e47fb3
Michael Veksler <mickey.veksler@gmail.com>
2026-06-15 16:13:54 +0300

    Implement an exponential solver
    This is a full-space search, it is intended as a baseline which will be rewritten soon.
    
    It makes it possible to implement and test:
    1. Storing variable assignment
    2. Literal checking
    
    Other things:
    1. Internal variable index starts with 1, to make things more consistent in the code.
    2. Solver returns Sat/Unsat not Option, since it can't fail at the moment.

--- tests/basic.rs
+++ tests/basic.rs
@@ -4,12 +4,50 @@ use solatic::{Solver, SolveResult};
 fn smoke_test() {
     let mut solver = Solver::new();
     solver.add_clause([1, -2, 3].as_slice());
-    assert_eq!(solver.solve().unwrap(), SolveResult::Sat);
+    assert_eq!(solver.solve(), SolveResult::Sat);
 }
 
 #[test]
 fn empty_clause_is_unsat() {
     let mut solver = Solver::new();
     solver.add_clause([].as_slice());
-    assert_eq!(solver.solve().unwrap(), SolveResult::Unsat);
+    assert_eq!(solver.solve(), SolveResult::Unsat);
+}
+
+#[test]
+fn nontrivial_sat() {
+    let mut solver = Solver::new();
+
+    // first we create mutual implication v1 -> !v2 -> v3 -> v1
+    // Remember the rule (a->b) is equivalent to (!a || b)
+    solver.add_clause(&[-1, -2]);  // v1 -> !v2
+    solver.add_clause(&[2, 3]);    // !v2 -> v3
+    solver.add_clause(&[-3, 1]);   // v3 -> v1
+
+    // now make it impossible to assign !v1, v2, !v3, leaving only v1, !v2, v3 as a solution
+    solver.add_clause(&[1, -2, 3]);
+
+    assert_eq!(solver.solve(), SolveResult::Sat);
+    assert_eq!(solver.value_of(1), Some(true));
+    assert_eq!(solver.value_of(2), Some(false));
+    assert_eq!(solver.value_of(3), Some(true));
+}
+
+#[test]
+fn conflict_is_unsat() {
+    let mut solver = Solver::new();
+
+    // first we create mutual implication v1 -> !v2 -> v3 -> v1
+    // Remember the rule (a->b) is equivalent to (!a || b)
+    solver.add_clause(&[-1, -2]);  // v1 -> !v2
+    solver.add_clause(&[2, 3]);    // !v2 -> v3
+    solver.add_clause(&[-3, 1]);   // v3 -> v1
+
+    // now make it impossible to assign !v1, v2, !v3, leaving only v1, !v2, v3 as a solution
+    solver.add_clause(&[1, -2, 3]);
+
+    // and finally forbid the only viable solution
+    solver.add_clause(&[-1, 2, -3]);
+
+    assert_eq!(solver.solve(), SolveResult::Unsat);
 }
