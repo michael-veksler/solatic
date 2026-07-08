eb80962169c7252c8009bbde73bd76cd100eef41
Michael Veksler <mickey.veksler@gmail.com>
2026-07-08 18:14:49 +0300

    Use more standard rust indexing and access

--- src/solver.rs
+++ src/solver.rs
@@ -1,5 +1,6 @@
 use bitflags::bitflags;
 use std::io;
+use std::ops::{Index, IndexMut};
 
 pub type Lit = i32;
 pub type ClauseId = u32;
@@ -37,7 +38,7 @@ impl ClauseDb {
         self.offsets.is_empty()
     }
 
-    pub fn add_clause(&mut self, lits: &[Lit]) -> ClauseId {
+    pub fn push(&mut self, lits: &[Lit]) -> ClauseId {
         let clause_id = self.offsets.len() as ClauseId;
         self.offsets.push(self.pool.len());
         let stored_size: Lit = lits.len().try_into().expect("clause too long");
@@ -46,7 +47,7 @@ impl ClauseDb {
         clause_id
     }
 
-    pub fn clause(&self, id: ClauseId) -> ClauseAccessor {
+    pub fn get(&self, id: ClauseId) -> ClauseAccessor {
         let header_pos = self.offsets[id as usize];
         let size = self.pool[header_pos] as usize;
         let begin = header_pos + 1;
@@ -118,32 +119,10 @@ struct WatchersDb {
     //        Maybe use unsafe code to manage pointers, or use thin-vec  or thin-dst / erasable
     pos_watchers: Vec<Vec<Watcher>>,
     neg_watchers: Vec<Vec<Watcher>>,
-    empty_watch: [Watcher; 0],
 }
 
 #[allow(dead_code)]
 impl WatchersDb {
-    pub fn watches(&self, lit: Lit) -> &[Watcher] {
-        let var = var_of(lit).expect("invalid literal");
-        let watchers = if is_pos(lit) {
-            &self.pos_watchers
-        } else {
-            &self.neg_watchers
-        };
-        watchers.get(var).map(|w| w.as_slice()).unwrap_or(&self.empty_watch)
-    }
-    pub fn update_watches(&mut self, lit: Lit) -> &mut Vec<Watcher> {
-        let var = var_of(lit).expect("invalid literal");
-        let watchers = if is_pos(lit) {
-            &mut self.pos_watchers
-        } else {
-            &mut self.neg_watchers
-        };
-        if var >= watchers.len() {
-            watchers.resize(var + 1, Vec::new());
-        }
-        &mut watchers[var]
-    }
     pub fn add_watch(&mut self, lit: Lit, watch: Watcher) {
         let var = var_of(lit).expect("invalid literal");
         let watchers = if is_pos(lit) {
@@ -155,6 +134,39 @@ impl WatchersDb {
             watchers.resize(var + 1, Vec::new());
         }
         watchers[var].push(watch);
+    }
+}
+
+impl Index<Lit> for WatchersDb {
+    type Output = Vec<Watcher>;
+    fn index(&self, lit: Lit) -> &Self::Output {
+        let var = var_of(lit).expect("invalid literal");
+        let watchers = if is_pos(lit) {
+            &self.pos_watchers
+        } else {
+            &self.neg_watchers
+        };
+        if let Some(watches) = watchers.get(var) {
+            watches
+        } else {
+            static EMPTY_WATCH: Vec<Watcher> = Vec::new();
+            &EMPTY_WATCH
+        }
+    }
+}
+
+impl IndexMut<Lit> for WatchersDb {
+    fn index_mut(&mut self, lit: Lit) -> &mut Self::Output {
+        let var = var_of(lit).expect("invalid literal");
+        let watchers = if is_pos(lit) {
+            &mut self.pos_watchers
+        } else {
+            &mut self.neg_watchers
+        };
+        if var >= watchers.len() {
+            watchers.resize(var + 1, Vec::new());
+        }
+        &mut watchers[var]
     }
 }
 
@@ -213,7 +225,7 @@ impl Solver {
         if let Some(max_var) = opt_max_var {
             self.ensure_vars(max_var);
         }
-        self.clauses.add_clause(lits);
+        self.clauses.push(lits);
     }
 
     fn literal_state(&self, lit: Lit) -> Assignment {
@@ -232,7 +244,7 @@ impl Solver {
 
     fn propagate_clause(&mut self, clause_id: ClauseId) -> PropagationResult {
         let mut free_literal: Option<Lit> = None;
-        for lit in self.clauses.literals(self.clauses.clause(clause_id)) {
+        for lit in self.clauses.literals(self.clauses.get(clause_id)) {
             let state = self.literal_state(*lit);
             if state == Assignment::POSITIVE {
                 return PropagationResult::Unchanged;
@@ -369,30 +381,30 @@ mod tests {
         let cl1 = [-1, -2, -3];
         let cl2 = [];
         let cl3 = [-5];
-        db.add_clause(cl0.as_slice());
-        db.add_clause(cl1.as_slice());
-        db.add_clause(cl2.as_slice());
-        db.add_clause(cl3.as_slice());
-        assert_eq!(db.literals(db.clause(0)), &cl0);
-        assert_eq!(db.literals(db.clause(1)), &cl1);
-        assert_eq!(db.literals(db.clause(2)), &cl2);
-        assert_eq!(db.literals(db.clause(3)), &cl3);
+        db.push(cl0.as_slice());
+        db.push(cl1.as_slice());
+        db.push(cl2.as_slice());
+        db.push(cl3.as_slice());
+        assert_eq!(db.literals(db.get(0)), &cl0);
+        assert_eq!(db.literals(db.get(1)), &cl1);
+        assert_eq!(db.literals(db.get(2)), &cl2);
+        assert_eq!(db.literals(db.get(3)), &cl3);
 
         let c4 = [5, 6];
-        db.add_clause(c4.as_slice());
-        assert_eq!(db.literals(db.clause(4)), &c4);
-        assert!(db.literals(db.clause(2)).is_empty());
-        assert!(!db.literals(db.clause(0)).is_empty());
+        db.push(c4.as_slice());
+        assert_eq!(db.literals(db.get(4)), &c4);
+        assert!(db.literals(db.get(2)).is_empty());
+        assert!(!db.literals(db.get(0)).is_empty());
 
-        db.literals_mut(db.clause(4))[0] = 8;
-        assert_eq!(db.literals(db.clause(4)), &[8, 6]);
+        db.literals_mut(db.get(4))[0] = 8;
+        assert_eq!(db.literals(db.get(4)), &[8, 6]);
     }
 
     #[test]
     fn test_watch_db() {
         let mut db = WatchersDb::default();
-        assert!(db.watches(1).is_empty());
-        assert!(db.watches(-1).is_empty());
+        assert!(db[1].is_empty());
+        assert!(db[-1].is_empty());
 
         db.add_watch(
             1,
@@ -417,7 +429,7 @@ mod tests {
         );
 
         assert_eq!(
-            db.watches(1),
+            db[1],
             &[
                 Watcher {
                     clause: 0,
@@ -430,19 +442,19 @@ mod tests {
             ]
         );
         assert_eq!(
-            db.watches(-1),
+            db[-1],
             &[Watcher {
                 clause: 1,
                 blocking_literal: -1
             }]
         );
 
-        db.update_watches(1).push(Watcher {
+        db[1].push(Watcher {
             clause: 3,
             blocking_literal: -8,
         });
         assert_eq!(
-            db.watches(1),
+            db[1],
             &[
                 Watcher {
                     clause: 0,
